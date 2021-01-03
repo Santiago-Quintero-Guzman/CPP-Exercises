549a704b8b05e2776fe4e10931b9df70145a977d
Santiago Quintero <sqgm84@gmail.com>
2021-01-03 03:28:45 -0500

    First commit.

---
+++ data_types/dataTypes.cxx
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+    int mInt;
+    long mLong;
+    char mChar;
+    float mFloat;
+    double mDouble;
+
+    scanf("%d %ld %c %f %lf", &mInt, &mLong, &mChar, &mFloat, &mDouble);
+    printf("%d \n%ld \n%c \n%.3f \n%.9lf \n", mInt, mLong, mChar, mFloat, mDouble);
+
+    return 0;
+}
